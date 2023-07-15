662d79eb654ee262920ac09f7d8a8311491f859a
Bartosz Malolepszy <bartosz.malolepszy@interia.com>
2023-07-15 22:02:05 +0200

    6.1 modyfied {} in whole code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-document.getElementById('play-rock').addEventListener('click', function () {
-  playGame(1);
-});
-document.getElementById('play-paper').addEventListener('click', function () {
-  playGame(2);
-});
-document.getElementById('play-scissors').addEventListener('click', function () {
-  playGame(3);
-});
-
+{
+  document.getElementById('play-rock').addEventListener('click', function () {
+    playGame(1);
+  });
+  document.getElementById('play-paper').addEventListener('click', function () {
+    playGame(2);
+  });
+  document
+    .getElementById('play-scissors')
+    .addEventListener('click', function () {
+      playGame(3);
+    });
+}
 /*document.getElementById('play-rock').addEventListener('click', const () {
   playGame(1);
 });
